6d5924b1711b9d20d68cc1575dd0bee75ab97cfa
Felipe Brasileiro <felipe.tanajura@gmail.com>
2020-11-05 22:37:38 -0300

    Added JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,21 @@
-const mongoose = require('mongoose');
-const catchAsync = require('../utils/catchAsync');
+const jwt = require('jsonwebtoken');
 const User = require('../Models/userModel');
+const catchAsync = require('../utils/catchAsync');
 
 
 exports.signup = catchAsync(async (req ,res, next)=>{
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm,
+    });
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_KEY, {expiresIn: process.env.JWT_EXPIRES_IN});
 
     res.status(201).json({
         status: 'success',
+        token: token,
         data:{
             user: newUser,
         }
